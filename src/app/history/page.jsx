5a044cff26ed9dc95036b2cf303880198d509c06
Vaibhav Jha <vaibhavjha22@gmail.com>
2024-05-05 21:02:29 +0530

    added table in milking history

--- src/app/history/page.jsx
+++ src/app/history/page.jsx
@@ -1,6 +1,24 @@
+"use client";
+
 import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useEffect, useState } from "react";
 
 export default function History() {
+  const [sessions, setSessions] = useState(null);
+
+  useEffect(() => {
+    let data = localStorage.getItem("sessions");
+    setSessions(JSON.parse(data));
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
       <div className="z-10 flex-col max-w-5xl w-full items-center font-mono text-sm flex">
@@ -13,8 +31,36 @@ export default function History() {
             Home
           </Link>
         </div>
-        <p className="text-3xl font-bold py-8">History</p>
-        <p>No milking yet!</p>
+        <p className="text-3xl font-bold py-8">Milking History</p>
+        {sessions === null ? (
+          <p>No milking yet!</p>
+        ) : (
+          <Table className="border-2 rounded-lg">
+            <TableCaption>A list of your recent milkings.</TableCaption>
+            <TableHeader>
+              <TableRow className="bg-slate-200">
+                <TableHead>Date</TableHead>
+                <TableHead>Start Time</TableHead>
+                <TableHead>End Time</TableHead>
+                <TableHead>Total Time</TableHead>
+                <TableHead className="text-right">Qty(l)</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {sessions?.map((session) => (
+                <TableRow key={session.id}>
+                  <TableCell className="font-medium">{session.date}</TableCell>
+                  <TableCell>{session.startTime}</TableCell>
+                  <TableCell>{session.endTime}</TableCell>
+                  <TableCell>{session.duration}</TableCell>
+                  <TableCell className="text-right">
+                    {session.quantity}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
       </div>
     </main>
   );
